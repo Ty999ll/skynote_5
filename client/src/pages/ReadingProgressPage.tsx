021f72d244e9f80666ee70c18ab319abaccb1f8d
Ty999ll <kimathityrell@gmail.com>
2025-07-13 20:25:11 +0300

    git commit -m "fix start reading button"

--- client/src/pages/ReadingProgressPage.tsx
+++ client/src/pages/ReadingProgressPage.tsx
@@ -192,6 +192,7 @@ export const ReadingProgressPage: React.FC = () => {
                   onClick={() => changeStatus(log.id, 'reading')}
                   className="text-xs"
                 >
+                  Start Reading
                 </Button>
               )}
               {log.status === 'reading' && (
